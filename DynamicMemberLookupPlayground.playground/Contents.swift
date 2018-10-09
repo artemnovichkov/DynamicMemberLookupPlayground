97dfed3f30f104ca42c69f05855f2ab5caf32b56
Artem Novichkov <novichkoff93@gmail.com>
2018-10-09 22:40:22 +0600

    Initial commit

---
+++ DynamicMemberLookupPlayground.playground/Contents.swift
@@ -0,0 +1,36 @@
+import UIKit
+import PlaygroundSupport
+
+class View: UIView {
+
+    private let selectors: Selectors = .init()
+
+    private(set) lazy var button: UIButton = {
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        button.setTitle("Tap", for: .normal)
+        button.addTarget(self, action: selectors.tap, for: .touchUpInside)
+        return button
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(button)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    @objc func tap() {
+        print("tap")
+    }
+}
+
+@dynamicMemberLookup
+struct Selectors {
+    subscript(dynamicMember member: String) -> Selector {
+        return Selector(member)
+    }
+}
+
+PlaygroundPage.current.liveView = View(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
